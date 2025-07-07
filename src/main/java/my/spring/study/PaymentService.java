129f0d6cbbe9d3cad719f50a60b3e92e40d6a7c3
이가희 (CT00418) <khlee@nextree.io>
2025-07-07 10:24:13 +0900

    refactor : 환율 가져오기 기능 클래스 분리

--- src/main/java/my/spring/study/PaymentService.java
+++ src/main/java/my/spring/study/PaymentService.java
@@ -4,20 +4,19 @@ import java.io.IOException;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 
-abstract  public class PaymentService {
+public class PaymentService {
+    private final WebApiExRatePaymentProvider exRateProvider;
+    // 요청이 올 때마다 새로 생성하지 않고, 한 번만 인스턴스화하여 재사용함
+
+    public PaymentService(){
+        this.exRateProvider = new WebApiExRatePaymentProvider();
+    }
+
     public Payment prepare(Long orderId, String currency, BigDecimal foreignCurrencyAmount) throws IOException {
-        BigDecimal exRate = getExRate(currency);
+        BigDecimal exRate = exRateProvider.getWebExRate(currency);
         BigDecimal convertedAmount = foreignCurrencyAmount.multiply(exRate);
         LocalDateTime validUntil = LocalDateTime.now().plusMinutes(30L);
 
         return new Payment(orderId, currency, foreignCurrencyAmount, exRate, convertedAmount, validUntil);
-    }
-
-    abstract BigDecimal getExRate(String currency) throws IOException;
-
-    public static void main(String[] args) throws IOException {
-        PaymentService paymentService = new SimpleExRatePaymentService();
-        Payment payment = paymentService.prepare(100L, "USD", BigDecimal.valueOf(50.7));
-        System.out.println(payment);
     }
 }
--- src/main/java/my/spring/study/SimpleExRateProvider.java
+++ src/main/java/my/spring/study/SimpleExRateProvider.java
@@ -3,8 +3,7 @@ package my.spring.study;
 import java.io.IOException;
 import java.math.BigDecimal;
 
-public class SimpleExRatePaymentService extends PaymentService {
-    @Override
+public class SimpleExRateProvider {
     BigDecimal getExRate(String currency) throws IOException {
         if (currency.equals("USD")) return BigDecimal.valueOf(1000);
 
